923a241d61f005facddd362d57abad3b47c849cb
Matt Willy <matt.willy@gmail.com>
2020-05-19 17:28:46 -0600

    pass the list through as props

--- src/App.js
+++ src/App.js
@@ -1,50 +1,56 @@
 import React from 'react';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1
-  },
-  {
-    title: 'Elixir',
-    url: 'https://elixir-lang.org/',
-    author: 'José Valim',
-    num_comments: 12,
-    points: 30,
-    objectID: 2
-  },
-  {
-    title: 'Phoenix',
-    url: 'https://www.phoenixframework.org/',
-    author: 'Chris McCord',
-    num_comments: 10,
-    points: 24,
-    objectID: 3
-  }
-];
 
 const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1
+    },
+    {
+      title: 'Elixir',
+      url: 'https://elixir-lang.org/',
+      author: 'José Valim',
+      num_comments: 12,
+      points: 30,
+      objectID: 2
+    },
+    {
+      title: 'Phoenix',
+      url: 'https://www.phoenixframework.org/',
+      author: 'Chris McCord',
+      num_comments: 10,
+      points: 24,
+      objectID: 3
+    }
+  ];
+
+  const handleChange = event => {
+    console.log(event.target.value);
+  };
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
-      <Search />
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
 
       <hr />
 
-      <List />
+      <List list={stories}/>
       
     </div>
   );
@@ -59,10 +65,10 @@ const Search = () => {
   );
 }
 
-const List = () => {
-  return list.map((item) => {
+const List = (props) => {
+  return props.list.map((item) => {
     return (
-        <div key={item.object}>
+        <div key={item.objectID}>
         <span>
           <a href={item.url}>
             {item.title}
