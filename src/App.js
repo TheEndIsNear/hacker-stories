544a7a9cc501556476e36c7aefcc066edd9e5208
Matt Willy <matt.willy@gmail.com>
2020-05-19 17:01:10 -0600

    display a list of objects

--- src/App.js
+++ src/App.js
@@ -1,21 +1,63 @@
 import React from 'react';
 
-function getTitle(title) {
-  return title;
-}
-
-function App() {
-  const welcome = {
-    greeting: 'Hey',
-    title: 'React'
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  },
+  {
+    title: 'Elixir',
+    url: 'https://elixir-lang.org/',
+    author: 'José Valim',
+    num_comments: 12,
+    points: 30,
+    objectID: 2
+  },
+  {
+    title: 'Phoenix',
+    url: 'https://www.phoenixframework.org/',
+    author: 'Chris McCord',
+    num_comments: 10,
+    points: 24,
+    objectID: 3
   }
-  
+];
+
+const App = () => {
   return (
     <div>
-      <h1>{welcome.greeting} {getTitle(welcome.title)}</h1>
+      <h1>My Hacker Stories</h1>
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
+
+      <hr />
+      {list.map((item) => {
+        return (
+           <div key={item.object}>
+            <span>
+              <a href={item.url}>
+                {item.title}
+              </a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+           </div>
+        );
+      })}
     </div>
   );
 }
